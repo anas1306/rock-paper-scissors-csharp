f65957d4e1f363a907f5fa66f232015ee58c8151
anas1306 <mohammed.anas1306@gmail.com>
2026-07-07 20:59:31 -0300

    Added random computer choice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ // function getComputerChoice() {
+ //     randomNum = Math.random()
+ //     if (randomNum < 0.33) {
+ //         computerChoice = "Rock";
+ //     } else if (randomNum >= 0.33 && randomNum <= 0.66) {
+ //         computerChoice = "Paper";
+ //     } else if (randomNum > 0.66) {
+ //         computerChoice = "Scissors";
+ //     }
+ //     return computerChoice;
+ // }
+ 
+static string GetComputerChoice() {
+    Random random = new Random();
+    int choice = random.Next(1, 4);
+    if (choice == 1) {
+        return "Rock";
+    } else if (choice == 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    } 
+} 
+Console.WriteLine(GetComputerChoice());
